6b179b024aa9519c72f1d7783e697dbe1f088d28
Rich <rich@posert.com>
2022-11-19 09:58:31 -0800

    Show list of correct words

--- static/heart-words-report.js
+++ static/heart-words-report.js
@@ -1,7 +1,22 @@
 const reportDiv = document.querySelector('#word-report');
 
 for (let student of Object.keys(reportInfo)) {
+
     let studentName = document.createElement('p');
-    studentName.innerHTML = `${student} got ${Object.values(reportInfo[student]).reduce((a, b) => (a + b))} words right.`;
+    const numCorrect = Object.values(reportInfo[student]).reduce((a, b) => (a + b));
+    studentName.innerHTML = `${student} got ${numCorrect} words right.`;
     reportDiv.appendChild(studentName);
+
+    if (numCorrect > 0) {
+        // make list of words
+        let wordContainer = document.createElement('div');
+        for (word of Object.keys(reportInfo[student])) {
+            if (reportInfo[student][word]) {
+                let wordEntry = document.createElement('p');
+                wordEntry.innerHTML = word;
+                wordContainer.appendChild(wordEntry);
+            }
+        }
+        reportDiv.appendChild(wordContainer);
+    }
 }
