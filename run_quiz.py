69c04063671a20f849ffa25ebc2dac39bfedd21e
Rich <rich@posert.com>
2023-08-29 14:49:08 -0700

    Rearch quiz

--- run_quiz.py
+++ run_quiz.py
@@ -9,44 +9,45 @@ import subprocess
 
 targets = list(ascii_letters)
 targets.extend(range(21))
-targets.extend(['cat', 'flip', 'think', 'because'])
+targets.extend(["cat", "flip", "think", "because"])
+
 
 class Database(object):
     def __init__(self, db_path):
         self.db_path = db_path
         try:
-            with open(db_path, 'r') as f:
+            with open(db_path, "r") as f:
                 self.db = json.load(f)
         except FileNotFoundError:
-            self.db = {'students': [], 'quizzes': {}, 'words': {}}
+            self.db = {"students": [], "quizzes": {}, "words": {}}
 
-        if 'words' not in self.db:
-            self.db['words'] = {}
+        if "words" not in self.db:
+            self.db["words"] = {}
 
     @property
     def students(self):
-        return self.db['students']
+        return self.db["students"]
 
     def new_student(self, student):
-        if student in self.db['students']:
-            return 'Student in list'
-        self.db['students'].append(student)
+        if student in self.db["students"]:
+            return "Student in list"
+        self.db["students"].append(student)
         self.save_db()
-        return 'Success'
+        return "Success"
 
     def delete_student(self, student):
-        if student not in self.db['students']:
-            return 'Student not in list'
-        self.db['students'].remove(student)
+        if student not in self.db["students"]:
+            return "Student not in list"
+        self.db["students"].remove(student)
         self.save_db()
-        return 'Success'
+        return "Success"
 
     @property
     def dates(self):
-        return self.db['quizzes'].keys()
+        return self.db["quizzes"].keys()
 
     def save_db(self):
-        with open(self.db_path, 'w') as f:
+        with open(self.db_path, "w") as f:
             json.dump(self.db, f)
 
     def new_quiz(self, student):
@@ -55,39 +56,37 @@ class Database(object):
         return True
 
     def complete_quiz(self, results):
-        student = results['student']
+        student = results["student"]
 
         today = str(date.today())
         if today not in self.dates:
-            self.db['quizzes'][today] = {
-                student: {x: False for x in targets}
-            }
+            self.db["quizzes"][today] = {student: {x: False for x in targets}}
         else:
-            self.db['quizzes'][today][student] = {x:False for x in targets}
+            self.db["quizzes"][today][student] = {x: False for x in targets}
 
-        for section in [results['upper'], results['lower']]:
+        for section in [results["upper"], results["lower"]]:
             for letter, correct in section.items():
-                self.db['quizzes'][today][student][letter] = correct
-            
-            for num, correct in results['numbers'].items():
-                num = int(num[1:])
-                self.db['quizzes'][today][student][num] = correct
+                self.db["quizzes"][today][student][letter] = correct
 
-            for word, correct in results['words'].items():
-                self.db['quizzes'][today][student][word] = correct
+            for num, correct in results["numbers"].items():
+                num = int(num[1:])
+                self.db["quizzes"][today][student][num] = correct
+
+            for word, correct in results["words"].items():
+                self.db["quizzes"][today][student][word] = correct
 
             self.save_db()
 
         return True
 
     def complete_heart_quiz(self, results):
-        student = results['student']
+        student = results["student"]
 
         today = str(date.today())
-        if student not in self.db['words']:
-            self.db['words'][student] = {}
+        if student not in self.db["words"]:
+            self.db["words"][student] = {}
 
-        self.db['words'][student][today] = results['correct_words']
+        self.db["words"][student][today] = results["correct_words"]
 
         self.save_db()
 
@@ -96,14 +95,14 @@ class Database(object):
     @property
     def heart_words_report(self):
         heart_words = {}
-        for student in self.db['words'].keys():
+        for student in self.db["words"].keys():
             try:
-                latest_quiz = max(self.db['words'][student].keys())
+                latest_quiz = max(self.db["words"][student].keys())
             except ValueError:
                 continue
             except KeyError:
                 print(student)
-            heart_words[student] = self.db['words'][student][latest_quiz]
+            heart_words[student] = self.db["words"][student][latest_quiz]
 
         return json.dumps(heart_words)
 
@@ -112,7 +111,7 @@ class Database(object):
         student_dates = {}
 
         # get all quiz dates
-        for date, quizzes in self.db['quizzes'].items():
+        for date, quizzes in self.db["quizzes"].items():
             for student in quizzes.keys():
                 if student not in self.students:
                     continue
@@ -128,166 +127,175 @@ class Database(object):
             student_dates[student] = student_dates[student][-1]
 
         for student, date in student_dates.items():
-            quiz = self.db['quizzes'][date][student]
+            quiz = self.db["quizzes"][date][student]
             success_dicts[student] = {}
-            success_dicts[student]['date'] = date
-            success_dicts[student]['results'] = quiz
+            success_dicts[student]["date"] = date
+            success_dicts[student]["results"] = quiz
 
-        return(success_dicts)
+        return success_dicts
 
     def make_df(self):
         csv_dicts = []
-        for date, quizzes in self.db['quizzes'].items():
+        for date, quizzes in self.db["quizzes"].items():
             for student, quiz in quizzes.items():
                 for target, results in quiz.items():
                     try:
                         for category, success in results.items():
-                            csv_dicts.append({
-                                'date': date,
-                                'student': student,
-                                'target': target,
-                                'category': category,
-                                'success': success
-                            })
+                            csv_dicts.append(
+                                {
+                                    "date": date,
+                                    "student": student,
+                                    "target": target,
+                                    "category": category,
+                                    "success": success,
+                                }
+                            )
                     except AttributeError:
-                        print('Attribute error when generating csv')
+                        print("Attribute error when generating csv")
                         continue
 
         return pd.DataFrame(csv_dicts)
 
     def update_record(self, rec):
-        target = rec['target']
-        assessment = rec['assess']
+        target = rec["target"]
+        assessment = rec["assess"]
 
-        if target in ['cat', 'flip', 'think', 'because'] and assessment == 'name':
-            assessment = 'read'
+        if target in ["cat", "flip", "think", "because"] and assessment == "name":
+            assessment = "read"
 
         print(target)
         print(assessment)
-        print(self.db['quizzes'][rec['date']][rec['student']][target])
+        print(self.db["quizzes"][rec["date"]][rec["student"]][target])
 
-        if assessment in self.db['quizzes'][rec['date']][rec['student']][target]:
-            self.db['quizzes'][rec['date']][rec['student']][target][assessment] = rec['correct']
+        if assessment in self.db["quizzes"][rec["date"]][rec["student"]][target]:
+            self.db["quizzes"][rec["date"]][rec["student"]][target][assessment] = rec[
+                "correct"
+            ]
             self.save_db()
             return True
         else:
             return False
 
     def get_seating_chart(self):
-        seating_chart = self.db.get('seating_chart')
+        seating_chart = self.db.get("seating_chart")
 
         if seating_chart is None:
-            seating_chart = {x:'drag-holder' for x in self.students}
+            seating_chart = {x: "drag-holder" for x in self.students}
         else:
             for student in self.students:
                 if student not in seating_chart:
-                    seating_chart[student] = 'drag-holder'
+                    seating_chart[student] = "drag-holder"
 
         return seating_chart
 
     def update_seating_chart(self, chart):
-        self.db['seating_chart'] = chart
+        self.db["seating_chart"] = chart
         self.save_db()
-        return 'OK'
+        return "OK"
 
 
-app = Flask(
-    __name__,
-    template_folder='templates'
-)
+app = Flask(__name__, template_folder="templates")
 
-db = Database('students.json')
+db = Database("students.json")
 
-@app.route('/', methods = ['GET'])
+
+@app.route("/", methods=["GET"])
 def index():
-    return render_template('index.html', students = db.students)
+    return render_template("index.html", students=db.students)
 
-@app.route('/quiz/<student>', methods = ['GET', 'POST'])
+
+@app.route("/quiz/<student>", methods=["GET", "POST"])
 def quiz(student):
-    if request.method == 'GET':
+    if request.method == "GET":
         db.new_quiz(student)
-        return render_template('quiz.html', student = student)
-    elif request.method == 'POST':
+        return render_template("quiz.html", student=student)
+    elif request.method == "POST":
         rq = request.get_json()
-        if rq['action'] == 'quiz_complete':
-            db.complete_quiz(rq)
-            return 'OK', 200
+        if rq["action"] == "quiz_complete":
+            print(rq)
+            # db.complete_quiz(rq)
+            return "OK", 200
 
-@app.route('/heart-words/<student>', methods = ['GET', 'POST'])
+
+@app.route("/heart-words/<student>", methods=["GET", "POST"])
 def heart_words(student):
-    if request.method == 'GET':
-        return render_template('heart-words.html', student = student)
-    elif request.method == 'POST':
+    if request.method == "GET":
+        return render_template("heart-words.html", student=student)
+    elif request.method == "POST":
         rq = request.get_json()
-        if rq['action'] == 'quiz_complete':
+        if rq["action"] == "quiz_complete":
             db.complete_heart_quiz(rq)
-            return 'OK', 200
+            return "OK", 200
 
-@app.route('/heart-words-report/', methods = ['GET'])
+
+@app.route("/heart-words-report/", methods=["GET"])
 def heart_words_report():
-    return render_template('heart-words-report.html', reportInfo = db.heart_words_report)
+    return render_template("heart-words-report.html", reportInfo=db.heart_words_report)
 
-@app.route('/api/<action>', methods = ['GET', 'POST'])
+
+@app.route("/api/<action>", methods=["GET", "POST"])
 def api(action):
-    if action == 'make-csv':
-        response = make_response(db.make_df().to_csv(index = False))
-        response.headers['Content-Disposition'] = 'attachment; filename=quiz-results.csv'
-        response.headers['Content-Type'] = 'text/csv'
+    if action == "make-csv":
+        response = make_response(db.make_df().to_csv(index=False))
+        response.headers[
+            "Content-Disposition"
+        ] = "attachment; filename=quiz-results.csv"
+        response.headers["Content-Type"] = "text/csv"
         return response
 
-    elif action == 'add-student':
-        print('Adding student')
+    elif action == "add-student":
+        print("Adding student")
         rq = request.get_json()
-        success = db.new_student(rq['student'])
-        return 'success' if success else 'failure', 200
+        success = db.new_student(rq["student"])
+        return "success" if success else "failure", 200
 
-    elif action == 'delete-student':
-        print('Deleting student')
+    elif action == "delete-student":
+        print("Deleting student")
         rq = request.get_json()
-        success = db.delete_student(rq['student'])
-        return 'success' if success else 'failure', 200
+        success = db.delete_student(rq["student"])
+        return "success" if success else "failure", 200
 
-    elif action == 'update-quiz':
-        print('Updating quiz')
+    elif action == "update-quiz":
+        print("Updating quiz")
         rq = request.get_json()
         success = db.update_record(rq)
-        return 'success' if success else 'failure', 200
+        return "success" if success else "failure", 200
 
-    elif action == 'student-seats':
-        return db.get_seating_chart(), 200, {'ContentType': 'application/json'}
+    elif action == "student-seats":
+        return db.get_seating_chart(), 200, {"ContentType": "application/json"}
 
-    elif action == 'update-seating-chart':
+    elif action == "update-seating-chart":
         new_chart = request.get_json()
         db.update_seating_chart(new_chart)
-        return 'OK', 200
+        return "OK", 200
 
-    elif action == 'make-graphs':
-        db.make_df().to_csv('student-results.csv', index = False)
-        subprocess.run([
-            'Rscript',
-            'make-graphs.R'
-        ])
-        
-        return render_template('student-plot.html')
+    elif action == "make-graphs":
+        db.make_df().to_csv("student-results.csv", index=False)
+        subprocess.run(["Rscript", "make-graphs.R"])
 
-    elif action == 'make-table':
-        db.make_df().to_csv('student-results.csv', index = False)
-        subprocess.run([
-            'Rscript',
-            'make-graphs.R'
-        ])
+        return render_template("student-plot.html")
 
-        return redirect('/static/student_table.html', code = 302)
+    elif action == "make-table":
+        db.make_df().to_csv("student-results.csv", index=False)
+        subprocess.run(["Rscript", "make-graphs.R"])
+
+        return redirect("/static/student_table.html", code=302)
 
     else:
-        print('bad request')
+        print("bad request")
         print(request.get_json())
 
-@app.route('/student-dashboard', methods = ['GET'])
+
+@app.route("/student-dashboard", methods=["GET"])
 def dashboard():
     success_strings = db.generate_report()
-    return render_template('dashboard.html', success_strings = json.dumps(success_strings))
+    return render_template(
+        "dashboard.html", success_strings=json.dumps(success_strings)
+    )
 
-@app.route('/seating-chart', methods = ['GET', 'POST'])
+
+@app.route("/seating-chart", methods=["GET", "POST"])
 def seating_chart():
-    return render_template('seating-chart.html', seating_chart = json.dumps(db.get_seating_chart()))
+    return render_template(
+        "seating-chart.html", seating_chart=json.dumps(db.get_seating_chart())
+    )
---
+++ static/run-quiz.js
@@ -0,0 +1,159 @@
+function sendRequest(body) {
+    return fetch(window.location, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify(body)
+    })
+}
+
+// from https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+
+const student = document.querySelector('#student-name').innerText;
+
+// Make arrays to keep track of targets
+
+const arrayLetters = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+
+let uppercaseLetterArray = []
+shuffle(arrayLetters).forEach(letter => {
+    uppercaseLetterArray.push({
+        'targetChar': letter,
+        'targetType': 'name',
+        'handwritten': false,
+        'success': false
+    });
+    uppercaseLetterArray.push({
+        'targetChar': letter,
+        'targetType': 'sound',
+        'handwritten': false,
+        'success': false
+    });
+})
+
+let lowercaseLetterArray = [];
+shuffle(arrayLetters).forEach(letter => {
+    letter = letter.toLocaleLowerCase();
+    lowercaseLetterArray.push({
+        'targetChar': letter,
+        'targetType': 'name',
+        'handwritten': letter === 'e' || letter === 'q',
+        'success': false
+    });
+})
+
+let integerArray = [];
+shuffle([...Array(21).keys()]).forEach(number => {
+    integerArray.push({
+        'targetChar': number,
+        'targetType': 'number',
+        'handwritten': false,
+        'success': false
+    });
+})
+
+let wordArray = [];
+// we don't want words to be shuffled
+['because', 'think', 'flip', 'cat'].forEach(word => {
+    wordArray.push({
+        'targetChar': word,
+        'targetType': 'word',
+        'handwritten': false,
+        'success': false
+    });
+})
+
+const targetArray = [
+    uppercaseLetterArray,
+    lowercaseLetterArray,
+    integerArray,
+    wordArray
+].reduce((accumulator, currentValue) => {
+    return accumulator.concat(currentValue);
+});
+
+// Determine result format
+
+function sendResults() {
+    sendRequest({
+        'action': 'quiz_complete',
+        'student': student,
+        'results': targetArray
+    });
+}
+
+// Run the quiz
+
+const bigText = document.querySelector('#quiz-target');
+const targetType = document.querySelector('#target-type');
+let currIndex = 0;
+let quizComplete = false;
+
+bigText.innerHTML = targetArray[currIndex].targetChar;
+targetType.innerHTML = targetArray[currIndex].targetType;
+
+function markAndAdvance(success) {
+    targetArray[currIndex]['success'] = success;
+    currIndex++;
+
+    const newTarget = targetArray[currIndex];
+    console.log(newTarget);
+
+    bigText.innerHTML = newTarget.targetChar;
+    targetType.innerHTML = newTarget.targetType;
+
+    // handle font types
+    bigText.classList.remove('other-handwritten');
+    bigText.classList.remove('arial');
+    if (newTarget.handwritten) {
+        console.log(newTarget.targetChar);
+        switch (newTarget.targetChar) {
+            case 'e':
+                bigText.classList.add('arial');
+                break;
+            case 'q':
+                bigText.classList.add('other-handwritten');
+                break;
+        }
+    }
+
+    if (currIndex == targetArray.length - 1) {
+        quizComplete = true;
+        bigText.innerHTML = 'Finished.';
+        targetType.innerHTML = '';
+        sendResults();
+    }
+}
+
+function handleKeypress(keyEvent) {
+    if (quizComplete) {
+        return;
+    }
+
+    switch (keyEvent.code) {
+        case 'ArrowUp':
+            markAndAdvance(true);
+            break;
+        case 'ArrowDown':
+            markAndAdvance(false);
+            break;
+    }
+}
+
+document.addEventListener('keyup', handleKeypress);
